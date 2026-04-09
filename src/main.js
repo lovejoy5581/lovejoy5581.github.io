f6ae78d7d12c3347d3f3b88b2417bc08b7c043dd
Lovejoy <shangjin857@gmail.com>
2026-04-09 18:37:03 +0800

    修复 Vue 和 Element Plus 导入问题

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import ElementPlus from "element-plus";
-import "element-plus/dist/index.css";
 import "./style.css";
 import "./assets/styles/global.css";
 import App from "./App.vue";
